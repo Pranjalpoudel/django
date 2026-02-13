05e54f7824f0076f086a725eed01088189e16a56
Pranjal Poudel <pranjalpaudel62@gmail.com>
2026-02-13 12:18:34 +0545

    solving issue

--- crud/urls.py
+++ crud/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("notes/", include("notes.urls")),
+    path('admin/', admin.site.urls),
+    path('notes/', include('notes.urls')),
 ]
